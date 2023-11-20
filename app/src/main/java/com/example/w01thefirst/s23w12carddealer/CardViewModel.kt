205f48a5c34dbc2c0a0b7d1eca0a4d731b0b6a5f
최연우 <50083524+wafla@users.noreply.github.com>
2023-11-20 20:56:08 +0900

    🚀 Compose Card Dealer 완성

---
+++ app/src/main/java/com/example/w01thefirst/s23w12carddealer/CardViewModel.kt
@@ -0,0 +1,26 @@
+package com.example.w01thefirst.s23w12carddealer
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import kotlin.random.Random
+
+class CardViewModel : ViewModel() {
+    private var _cards = MutableLiveData<IntArray>(IntArray(5){0})
+    val cards: LiveData<IntArray>
+        get() = _cards
+    fun shuffle(){
+        var num = 0
+        val newCards = IntArray(5){-1}
+
+        for(i in newCards.indices){
+            do{
+                num = Random.nextInt(52)
+            }while(newCards.contains(num))
+            newCards[i]=num
+        }
+
+        newCards.sort()
+        _cards.value = newCards
+    }
+}
